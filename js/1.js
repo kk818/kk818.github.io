bb8f439340829506b858dfa759d0dcf5428d9301
kk818 <315988276@qq.com>
2017-02-09 11:48:07 +0800

    新增总结初稿和JS文件

---
+++ js/1.js
@@ -0,0 +1,5 @@
+$(function(){			
+			$("#text").text('<script src="http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>')
+			var cssobj={"background-color":"rgb(245,245,245)","width":"100%"}
+			$("#text").css(cssobj)
+});
